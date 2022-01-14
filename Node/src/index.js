481d6631127bf1b8a80f6cbd3fc0ba076d27d40c
LB-Okami <lucabtei@outlook.com>
2022-01-14 00:22:27 -0300

    first commit �ð¦

---
+++ Node/src/index.js
@@ -0,0 +1,27 @@
+const http = require('http')
+
+const routes = require('./routes')
+const url = require('url')
+
+const server = http.createServer((request, response) => {
+    const parsedUrl = url.parse(request.url, true)
+    console.log(parsedUrl)
+
+    console.log(`Request Method: ${request.method} | Endpoint: ${parsedUrl.pathname}`)
+    
+    const route = routes.find((routeObj) => (
+        routeObj.endpoint === parsedUrl.pathname && routeObj.method === request.method
+    ));
+
+    if(route) {
+        request.query = parsedUrl.query
+        route.handler(request, response )
+    }
+    else {
+        response.writeHead(404, { 'Content-Type': 'text/html' })
+        response.end(`Cannot ${request.method} ${parsedUrl.pathname}`)
+    }
+
+})
+
+server.listen(8080, () => console.log("🔥 Server running at http://localhost:8080"))
---
+++ Node/src/mocks/users.js
@@ -0,0 +1,18 @@
+module.exports = [
+    {
+        id: 1, 
+        name: "Pedro"
+    },
+    {
+        id: 2, 
+        name: "Marcos"
+    },
+    {
+        id: 3, 
+        name: "Júlia"
+    },
+    {
+        id: 4, 
+        name: "Lucas"
+    }
+]
